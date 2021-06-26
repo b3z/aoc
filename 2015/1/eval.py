82928ffd6c84ea6565f20aae69b2333e1151d46f
b3z <b3z@zeppel.eu>
2021-06-27 01:56:32 +0200

    15 1 2

--- 2015/1/eval.py
+++ 2015/1/eval.py
@@ -3,13 +3,22 @@
 input = "input.txt"
 result = 0
 
+idx = 0
+firstBasementIdx = -1
+
 with open(input) as inst:
     for line in inst:
         for c in line:
+            idx += 1
             if c == '(': # up one floor
                 result += 1
             else:
                 result -= 1 # down one floor (case ')')
 
+            if firstBasementIdx == -1: # did we find a first basement yet? true == no.
+                if result == -1: # first basement.
+                    firstBasementIdx = idx # set index of first basement.
+
 print(result)
+print(f'First basement index: {firstBasementIdx}')
                 
