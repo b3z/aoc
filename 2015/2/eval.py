f4e3a57af39e621b6026a13e4f27205a896132a4
b3z <b3z@zeppel.eu>
2021-06-27 02:24:33 +0200

    15 2 2

--- 2015/2/eval.py
+++ 2015/2/eval.py
@@ -4,6 +4,8 @@ inp = "input.txt"
 
 total = 0 # total squarefeet of wrapping paper
 
+ribbon = 0 # feet of ribbon
+
 with open(inp) as data:
     for line in data:
         l, w, h = line.split('x')
@@ -12,7 +14,17 @@ with open(inp) as data:
         w = int(w)
         h = int(h)
 
+        # wrapping paper
         total += 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)# box surface + slack
 
-print(total)
+        # ribbon
+        t = [l, w, h] # get two smallest
+        a = min(t)
+        t.remove(a)
+        b = min(t)
 
+        ribbon += 2*a + 2*b + l*w*h
+
+
+print(total)
+print(f'Ribbon {ribbon}')
