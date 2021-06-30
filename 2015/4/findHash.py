774d05ff21efa00e9c22dc1d5435caf0f5277642
b3z <b3z@zeppel.eu>
2021-06-30 19:16:22 +0200

    15 4 1

---
+++ 2015/4/findHash.py
@@ -0,0 +1,20 @@
+import hashlib
+
+def hash(s):
+    return hashlib.md5(s.encode()).hexdigest()
+
+
+input = "iwrupvqb"
+#input = "abcdef" # expect 609043
+#input = "pqrstuv" # expect 1048970
+num = 0
+res = hash(input)
+
+while res[:5] != "00000":
+    res = hash(input+str(num))
+    num += 1
+    print(res)
+
+print(f'Result: {input} {str(num-1)}')
+
+
