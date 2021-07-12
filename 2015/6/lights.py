0ceeeb838134baec2491d6a93d5f87165847ef3b
b3z <b3z@zeppel.eu>
2021-07-12 10:47:24 +0200

    15 6 1

---
+++ 2015/6/lights.py
@@ -0,0 +1,59 @@
+
+class Grid:
+    def __init__(self):
+        self.grid = [[0 for x in range(1000)] for y in range(1000)]  # 1000 x 1000 grid
+
+    def set(self, c1, c2, value):
+        while c1[0] <= c2[0]:
+            tmp = c1[1]
+            while tmp <= c2[1]:
+                self.grid[c1[0]][tmp] = value
+                tmp += 1
+            c1[0] += 1
+
+    def on(self, c1, c2):
+        self.set(c1, c2, 1)
+
+    def off(self, c1, c2):
+        self.set(c1, c2, 0)
+
+    # aka invert
+    def toggle(self, c1, c2):
+        while c1[0] <= c2[0]:
+            tmp = c1[1]
+            while tmp <= c2[1]:
+                state = self.grid[c1[0]][tmp]
+                if state == 0:
+                    self.grid[c1[0]][tmp] = 1
+                else:
+                    self.grid[c1[0]][tmp] = 0
+                tmp += 1
+            c1[0] += 1
+
+    def print(self):
+        for row in self.grid:
+            print(row)
+
+    def countOn(self):
+        numOn = 0
+        for row in self.grid:
+            numOn += sum(row)
+        return numOn
+
+g = Grid()
+
+with open("input.txt") as data:
+    for line in data:
+        inst = line.split(' ')
+        
+        c1 = list(map(int, inst[-3].split(',')))
+        c2 = list(map(int, inst[-1].split(',')))
+
+        if inst[0] == "toggle":
+            g.toggle(c1, c2)
+        elif inst[1] == "on":
+            g.on(c1, c2)
+        else:
+            g.off(c1, c2)
+
+print(f'Lights on: {g.countOn()}')
