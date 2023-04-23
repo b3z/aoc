5aa4ad168e50f30274d0b701e8ae49322cd2c575
b3z <b3z@zeppel.eu>
2023-04-23 22:58:25 +0200

    part two

---
+++ 2015/01/2.py
@@ -0,0 +1,19 @@
+lines = open("input").readlines()
+
+floor = 0
+f = 1
+
+for l in lines[0]:
+    if l == '(':
+        floor += 1
+    elif l == ')':
+        floor -= 1
+
+    if floor < 0:
+        break
+    
+    f += 1
+
+print(f)
+
+
