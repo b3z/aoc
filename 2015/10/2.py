ac1277a746f3a8d27edf0f0fa2e26bbe4561f3a9
b3z <b3z@zeppel.eu>
2023-10-04 22:11:17 +0200

    2015 10

---
+++ 2015/10/2.py
@@ -0,0 +1,28 @@
+inp = "1113222113"
+
+def run(s):
+    c = 0
+    a = None
+    res = []
+    for i in range(len(s)):
+        if a == None:
+            a = s[i]
+            c += 1
+            continue
+
+        if s[i] == a:
+            c += 1
+
+        else:
+            res.append(str(c) + a)
+            a = s[i]
+            c = 1
+
+    res.append(str(c) + a)
+
+    return ''.join(res)
+
+for s in range(50):
+    inp = run(inp)
+
+print(len(inp))
