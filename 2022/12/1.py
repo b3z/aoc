a0473aeb9303c525e8d251ea8f328cfbe8a1b599
b3z <b3z@zeppel.eu>
2023-10-21 20:37:46 +0200

    2022 12

---
+++ 2022/12/1.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+file = open('input')
+grid = [list(x) for x in file.read().strip().splitlines()]
+
+for r, a in enumerate(grid):
+    for c, b in enumerate(a):
+        if b == 'S':
+            sp = (r, c)
+            grid[r][c] = 'a'
+        if b == 'E':
+            ep = (r, c)
+            grid[r][c] = 'z'
+
+q = deque()
+q.append((0, sp[0], sp[1]))
+
+vis = {sp}
+
+while q:  # as long q not empty
+    d, r, c = q.popleft()
+    for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+        if nr < 0 or nc < 0 or nr >= len(grid) or nc >= len(grid[0]):
+            continue
+
+        if (nr, nc) in vis:
+            continue
+
+        # print(nr, nc)
+        if ord(grid[nr][nc]) - ord(grid[r][c]) > 1:
+            continue
+
+        if (nr, nc) == ep:
+            print(d + 1)
+            exit(0)
+
+        vis.add((nr, nc))
+        q.append((d + 1, nr, nc))
