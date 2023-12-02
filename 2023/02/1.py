aeb9b5c1727350f4ae8aef3f3c97194ea9e7b989
b3z <b3z@zeppel.eu>
2023-12-02 20:39:08 +0100

    day two

---
+++ 2023/02/1.py
@@ -0,0 +1,20 @@
+f = open("input")
+
+sum = 0
+
+for g in f:
+    sx = []
+    id = int(g.split(": ")[0].split(" ")[1])
+    g = g.strip().split(": ")[1].split("; ")
+    for s in g:
+        a = {"red": 0, "green": 0, "blue": 0}
+        for r in s.split(", "):
+            n, c = r.split(" ")
+            a[c] = int(n)
+
+        if a["red"] > 12 or a["green"] > 13 or a["blue"] > 14:
+            break
+    else:
+        sum += id
+
+print(sum)
---
+++ 2023/02/2.py
@@ -0,0 +1,23 @@
+from functools import reduce
+from operator import mul
+
+f = open("input")
+
+sum = 0
+
+for g in f:
+    res = {"red": 0, "green": 0, "blue": 0}
+    id = int(g.split(": ")[0].split(" ")[1])
+    g = g.strip().split(": ")[1].split("; ")
+    for s in g:
+        a = {"red": 0, "green": 0, "blue": 0}
+        for r in s.split(", "):
+            n, c = r.split(" ")
+            a[c] = int(n)
+
+        for i in a.items():
+            res[i[0]] = max(res[i[0]], i[1])
+
+    sum += reduce(mul, res.values(), 1)
+
+print(sum)
